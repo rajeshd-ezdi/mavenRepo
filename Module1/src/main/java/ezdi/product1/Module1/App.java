b093b2facbfa4efc21adb8ca572a15a3668ad2cc
rajeshd-ezdi <rajesh.d@ezdi.us>
2017-05-18 15:58:44 +0530

    Rajesh

---
+++ Module1/src/main/java/ezdi/product1/Module1/App.java
@@ -0,0 +1,13 @@
+package ezdi.product1.Module1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
